c5112f60796237d181fec3ba2e844a440f91f1dc
Elyézer Rezende <elyezermr@gmail.com>
2017-08-09 18:41:33 -0300

    Add isolated_filesystem context manager
    The context manager helps changing the current working directory to a
    temporary directory that will be removed later.

--- camayoc/utils.py
+++ camayoc/utils.py
@@ -1,8 +1,32 @@
 # coding=utf-8
 """Utility functions."""
+import contextlib
+import os
+import shutil
+import tempfile
 import uuid
 
 
 def uuid4():
     """Return a random UUID, as a unicode string."""
     return str(uuid.uuid4())
+
+
+@contextlib.contextmanager
+def isolated_filesystem():
+    """Context Manager that creates a temporary directory.
+
+    Changes the current working directory to the created temporary directory
+    for isolated filesystem tests.
+    """
+    cwd = os.getcwd()
+    path = tempfile.mkdtemp()
+    os.chdir(path)
+    try:
+        yield path
+    finally:
+        os.chdir(cwd)
+        try:
+            shutil.rmtree(path)
+        except (OSError, IOError):
+            pass
