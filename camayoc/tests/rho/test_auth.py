b9e9c6292633d36eca42eeb932bfef3e108a3194
Elyézer Rezende <elyezermr@gmail.com>
2017-08-14 15:02:23 -0300

    Move utility functions to a separate module
    Move the utility functions used by RHO tests into a separated Python
    module.

--- camayoc/tests/rho/test_auth.py
+++ camayoc/tests/rho/test_auth.py
@@ -18,50 +18,8 @@ from camayoc import utils
 from camayoc.constants import (
     MASKED_PASSWORD_OUTPUT,
     PASSWORD_INPUT,
-    VAULT_PASSWORD,
-    VAULT_PASSWORD_INPUT,
 )
-
-
-def input_vault_password(process, vault_password=VAULT_PASSWORD):
-    """Expect the vault password input and input the vault password.
-
-    :param process: A pexpect object returned by the ``pexpect.spawn``.
-    :param vault_password: The vault password to be used, defaults to
-        :ref:`camayoc.constants.VAULT_PASSWORD`.
-    """
-    assert process.expect(VAULT_PASSWORD_INPUT) == 0
-    process.sendline(vault_password)
-
-
-def auth_add(
-        options, inputs=None, exitstatus=0, vault_password=VAULT_PASSWORD):
-    """Add a new auth entry.
-
-    :param options: A dictionary mapping the option names and their values.
-        Pass ``None`` for flag options.
-    :param inputs: A dictionary mapping the input prompts and the value to be
-        filled.
-    :param exitstatus: Expected exit status code.
-    :param vault_password: The vault password to be used, defaults to
-        :ref:`camayoc.constants.VAULT_PASSWORD`.
-    """
-    command = 'rho auth add'
-    for key, value in options.items():
-        if value is None:
-            command += ' --{}'.format(key)
-        else:
-            command += ' --{}={}'.format(key, value)
-    rho_auth_add = pexpect.spawn(command)
-    input_vault_password(rho_auth_add, vault_password)
-    if inputs is None:
-        inputs = {}
-    for key, value in inputs.items():
-        assert rho_auth_add.expect(key) == 0
-        rho_auth_add.sendline(value)
-    assert rho_auth_add.expect(pexpect.EOF) == 0
-    rho_auth_add.close()
-    assert rho_auth_add.exitstatus == exitstatus
+from camayoc.tests.rho.utils import auth_add, input_vault_password
 
 
 def test_add_with_username_password(isolated_filesystem):
