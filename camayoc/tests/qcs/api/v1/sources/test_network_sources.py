f932c2a83cf3d36c09eb9154628e4f217a537a84
Chris Hambridge <chambrid@redhat.com>
2018-03-26 12:43:01 -0400

    Add tests focusing on network sources. Closes #170.

--- camayoc/tests/qcs/api/v1/sources/test_network_sources.py
+++ camayoc/tests/qcs/api/v1/sources/test_network_sources.py
@@ -9,10 +9,31 @@
 :testtype: functional
 :upstream: yes
 """
+import copy
+from pathlib import Path
+
 import pytest
 
+from camayoc import api
+from camayoc.utils import uuid4
+from camayoc.qcs_models import (
+    Credential,
+    Source,
+)
 
-@pytest.mark.skip
+from camayoc.tests.qcs.utils import (
+    assert_matches_server,
+    assert_source_update_fails,
+)
+
+NETWORK_TYPE = 'network'
+CREATE_DATA = ['localhost', '127.0.0.1', 'example.com']
+HOST_FORMAT_DATA = [['192.0.2.[0:255]', '192.0.3.0/24']]
+RESULTING_HOST_FORMAT_DATA = ['192.0.2.[0:255]', '192.0.3.[0:255]']
+MIXED_DATA = CREATE_DATA + HOST_FORMAT_DATA
+
+
+@pytest.mark.parametrize('scan_host', HOST_FORMAT_DATA)
 def test_create_multiple_hosts(shared_client, cleanup, scan_host):
     """Create a Network Source using multiple hosts.
 
@@ -26,10 +47,27 @@ def test_create_multiple_hosts(shared_client, cleanup, scan_host):
     :expectedresults: The source is created.
     :caseautomation: notautomated
     """
-    pass
+    cred = Credential(
+        cred_type=NETWORK_TYPE,
+        client=shared_client,
+        password=uuid4()
+    )
+    cred.create()
+    src = Source(
+        source_type=NETWORK_TYPE,
+        client=shared_client,
+        hosts=scan_host,
+        credential_ids=[cred._id],
+    )
+    src.create()
+    src.hosts = RESULTING_HOST_FORMAT_DATA
+    assert_matches_server(src)
+
+    # add the ids to the lists to destroy after the test is done
+    cleanup.extend([cred, src])
 
 
-@pytest.mark.skip
+@pytest.mark.parametrize('scan_host', CREATE_DATA)
 def test_create_multiple_creds(shared_client, cleanup, scan_host):
     """Create a Network Source using multiple credentials.
 
@@ -41,10 +79,36 @@ def test_create_multiple_creds(shared_client, cleanup, scan_host):
     :expectedresults: The source is created.
     :caseautomation: notautomated
     """
-    pass
+    ssh_keyfile = Path(uuid4())
+    ssh_keyfile.touch()
+
+    ssh_key_cred = Credential(
+        cred_type=NETWORK_TYPE,
+        client=shared_client,
+        ssh_keyfile=str(ssh_keyfile.resolve()),
+    )
+    ssh_key_cred.create()
+    pwd_cred = Credential(
+        cred_type=NETWORK_TYPE,
+        client=shared_client,
+        password=uuid4()
+    )
+    pwd_cred.create()
+
+    src = Source(
+        source_type=NETWORK_TYPE,
+        client=shared_client,
+        hosts=[scan_host],
+        credential_ids=[ssh_key_cred._id, pwd_cred._id],
+    )
+    src.create()
+    # add the ids to the lists to destroy after the test is done
+    cleanup.extend([ssh_key_cred, pwd_cred, src])
+
+    assert_matches_server(src)
 
 
-@pytest.mark.skip
+@pytest.mark.parametrize('scan_host', MIXED_DATA)
 def test_create_multiple_creds_and_sources(shared_client, cleanup, scan_host):
     """Create a Network Source using multiple credentials.
 
@@ -58,10 +122,41 @@ def test_create_multiple_creds_and_sources(shared_client, cleanup, scan_host):
     :expectedresults: The source is created.
     :caseautomation: notautomated
     """
-    pass
+    ssh_keyfile = Path(uuid4())
+    ssh_keyfile.touch()
+
+    ssh_key_cred = Credential(
+        cred_type=NETWORK_TYPE,
+        client=shared_client,
+        ssh_keyfile=str(ssh_keyfile.resolve()),
+    )
+    ssh_key_cred.create()
+    pwd_cred = Credential(
+        cred_type=NETWORK_TYPE,
+        client=shared_client,
+        password=uuid4()
+    )
+    pwd_cred.create()
+
+    input_hosts = [scan_host]
+    if isinstance(scan_host, list):
+        input_hosts = scan_host
+    src = Source(
+        source_type=NETWORK_TYPE,
+        client=shared_client,
+        hosts=input_hosts,
+        credential_ids=[ssh_key_cred._id, pwd_cred._id],
+    )
+    src.create()
+    if isinstance(scan_host, list):
+        src.hosts = RESULTING_HOST_FORMAT_DATA
+    assert_matches_server(src)
+
+    # add the ids to the lists to destroy after the test is done
+    cleanup.extend([ssh_key_cred, pwd_cred, src])
 
 
-@pytest.mark.skip
+@pytest.mark.parametrize('scan_host', CREATE_DATA)
 def test_negative_update_invalid(shared_client, cleanup, scan_host):
     """Create a network source and then update it with invalid data.
 
@@ -74,4 +169,37 @@ def test_negative_update_invalid(shared_client, cleanup, scan_host):
     :expectedresults: An error is thrown and no new host is created.
     :caseautomation: notautomated
     """
-    pass
+    ssh_keyfile = Path(uuid4())
+    ssh_keyfile.touch()
+
+    net_cred = Credential(
+        cred_type=NETWORK_TYPE,
+        client=shared_client,
+        ssh_keyfile=str(ssh_keyfile.resolve()),
+    )
+    net_cred.create()
+    sat_cred = Credential(
+        cred_type='satellite',
+        client=shared_client,
+        password=uuid4()
+    )
+    sat_cred.create()
+
+    src = Source(
+        source_type=NETWORK_TYPE,
+        client=shared_client,
+        hosts=[scan_host],
+        credential_ids=[net_cred._id],
+    )
+    src.create()
+    original_data = copy.deepcopy(src.fields())
+    src.client = api.Client(api.echo_handler)
+
+    # Try to update with invalid credential type
+    src.credentials = [sat_cred._id]
+    assert_source_update_fails(original_data, src)
+    src.hosts = ['1**2@33^']
+    assert_source_update_fails(original_data, src)
+
+    # add the ids to the lists to destroy after the test is done
+    cleanup.extend([net_cred, sat_cred, src])
