7f5b507d365367b4eff2be488dd893763ca037f5
Chris Hambridge <chambrid@redhat.com>
2018-04-11 15:58:46 -0400

    Skip ui test if driver is not configured. Closes #229. (#233)

--- camayoc/tests/qpc/ui/conftest.py
+++ camayoc/tests/qpc/ui/conftest.py
@@ -6,6 +6,20 @@ from widgetastic.browser import Browser
 from camayoc.utils import get_qpc_url
 
 
+def pytest_collection_modifyitems(config, items):
+    """Check for presence of driver for selenium.
+
+    If no driver option is present then UI test should be skipped.
+    UI tests should be marked with @pytest.mark.ui
+    """
+    if not config.getoption('--driver', None):
+        skip_ui = pytest.mark.skip(
+            reason='need --driver option to run UI tests')
+        for item in items:
+            if 'ui' in item.fspath.strpath:
+                item.add_marker(skip_ui)
+
+
 @pytest.fixture
 def browser(selenium):
     """Widgetastic browser fixture."""
