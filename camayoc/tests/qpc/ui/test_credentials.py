07f5417bb1ef66c29e6471714ba42f768ce644ff
Daniel Sami <danielalbertsami@gmail.com>
2018-08-13 13:20:09 -0400

    docstring fix

--- camayoc/tests/qpc/ui/test_credentials.py
+++ camayoc/tests/qpc/ui/test_credentials.py
@@ -156,7 +156,7 @@ def test_edit_credential(browser, qpc_login, credential_type):
         2) Go to the credentials page and open the Add Credential modal.
         3) Fill required + optional fields, using the SSH key option and save.
         4) Edit the credential parameters and save, make sure it changed.
-        4) Delete the newly created credential.
+        5) Delete the newly created credential.
     :expectedresults: A new credential is created, edited, then deleted.
     """
     options = {
