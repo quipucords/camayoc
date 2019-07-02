a5788a35308284f3872804b717101b87010b5787
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-07-02 15:31:47 -0400

    Cleaned up and added test to send file to upload-service

--- camayoc/tests/qpc/yupana/test_connect.py
+++ camayoc/tests/qpc/yupana/test_connect.py
@@ -2,7 +2,12 @@
 
 import requests
 
-from camayoc.tests.qpc.yupana.utils import yupana_configs, get_api_url
+from camayoc.tests.qpc.yupana.utils import (
+    yupana_configs,
+    get_app_url,
+    post_file,
+    get_x_rh_identity,
+)
 
 
 EXPECTED_CONFIGS = ("app_name", "openshift_project", "openshift_app_domain", "api_path")
@@ -12,10 +17,25 @@ def test_config():
     """test load configurations"""
     config = yupana_configs()
     assert config != []
-    assert sorted(set(config.keys())) == sorted(EXPECTED_CONFIGS)
+    assert sorted(set(config["yupana-app"].keys())) == sorted(EXPECTED_CONFIGS)
 
 
 def test_connect():
     """Test the connection to the desired cluster."""
-    request = requests.get(get_api_url() + "status/")
+    request = requests.get(get_app_url() + "status/")
     assert request.status_code == 200
+
+
+def test_upload():
+    """Test uploading a file to the service."""
+    yupana_config = yupana_configs()
+    config = yupana_config["upload-service"]
+    response = post_file(
+        config["file_upload_src"],
+        config["api_url"],
+        config["rh_username"],
+        config["rh_password"],
+        get_x_rh_identity(config["rh_account_number"], config["rh_org_id"]),
+        config["rh_insights_request_id"],
+    )
+    assert response.status_code == 202
--- camayoc/tests/qpc/yupana/utils.py
+++ camayoc/tests/qpc/yupana/utils.py
@@ -2,6 +2,10 @@
 """Utility functions for yupana tests."""
 
 
+import base64
+import json
+import requests
+
 from camayoc.config import get_config
 from camayoc.exceptions import ConfigFileNotFoundError, WaitTimeError
 
@@ -15,9 +19,21 @@ def yupana_configs():
     return yupana_configs
 
 
-def get_api_url():
+def get_x_rh_identity(account_num, org_id):
+    """Return the base64 encoded x-rh-identity string from credentials."""
+    login_data = {
+        "identity": {"account_number": account_num, "internal": {"org_id": org_id}}
+    }
+    print(login_data)
+    json_str = json.dumps(login_data)
+    print(json_str)
+    return base64.b64encode(json_str.encode("ascii"))
+
+
+def get_app_url(protocol="http://"):
     """Build and return the URI from the config to access the yupana API"""
-    config = yupana_configs()
+    yupana_config = yupana_configs()
+    config = yupana_config["yupana-app"]
     required_configs = (
         "app_name",
         "openshift_project",
@@ -26,7 +42,7 @@ def get_api_url():
     )
     if set(required_configs).issubset(set(config.keys())):
         url = (
-            "http://"
+            protocol
             + config["app_name"]
             + "-"
             + config["openshift_project"]
@@ -37,3 +53,31 @@ def get_api_url():
         return url
     else:
         return None
+
+
+def post_file(
+    file_src,
+    api_url,
+    rh_username,
+    rh_pass,
+    x_rh_identity,
+    x_rh_insights_request_id,
+    file_type="application/vnd.redhat.qpc.tar+tgz",
+):
+    """Send a file/package up to the cluster."""
+
+    headers = {
+        "x-rh-insights-request-id": x_rh_insights_request_id,
+        "x-rh-identity": x_rh_identity,
+    }
+
+    files = {"file": (file_src, open(file_src, "rb"), file_type)}
+    api_endpoint = api_url + "/upload"
+    response = requests.post(
+        api_endpoint,
+        headers=headers,
+        files=files,
+        auth=(rh_username, rh_pass),
+        verify=False,
+    )
+    return response
