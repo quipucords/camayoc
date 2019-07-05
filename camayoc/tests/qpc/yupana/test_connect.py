997351689c67263ef09b4614dc781c53bde8364a
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-07-05 16:51:29 -0400

    Updated docstrings to meet requirements

--- camayoc/tests/qpc/yupana/test_connect.py
+++ camayoc/tests/qpc/yupana/test_connect.py
@@ -1,4 +1,14 @@
-"""Starting tests for configs"""
+# coding=utf-8
+"""Tests for connecting to yupana and related cluster apps
+
+:caseautomation: automated
+:casecomponent: yupana
+:caseimportance: high
+:caselevel: integration
+:requirement: Sonar
+:testtype: functional
+:upstream: yes
+"""
 
 import requests
 
@@ -28,7 +38,16 @@ EXPECTED_UPLOAD_SERVICE_CONFIGS = (
 
 
 def test_yupana_config():
-    """test load configurations"""
+    """Ensure the required yupana app configs are provided.
+
+    :id: 94ed56dc-9f65-11e9-aaaf-8c1645a90ee2
+    :description: Ensure that the required configs to test the yupana app are
+        defined.
+    :expectedresults: Configs should be defined in the 'yupana->yupana-app'
+        section of the camayoc configuration file. Values should be supplied for
+        each item defined in 'EXPECTED_APP_CONFIGS'.
+    """
+
     config = yupana_configs()
     assert config != []
     assert sorted(set(config["yupana-app"].keys())) == sorted(
@@ -37,21 +56,42 @@ def test_yupana_config():
 
 
 def test_upload_service_config():
-    """test upload service config provided"""
+    """Ensure the required upload service configs are provided.
+
+    :id: f44b7cda-9f65-11e9-8acb-8c1645a90ee2
+    :description: Ensure that the required configs to test interacting with the
+        upload service are defiend.
+    :expectedresults: Configs should be defined in the 'yupana->yupana-app'
+        section of the camayoc configuration file. Values should be supplied for
+        each key item defined in 'EXPECTED_UPLOAD_SERVICE_CONFIGS'.
+    """
     config = yupana_configs()
     assert sorted(set(config["upload-service"].keys())) == sorted(
         EXPECTED_UPLOAD_SERVICE_CONFIGS
     ), "Expected upload service configs not properly provided."
 
 
-def test_connect():
-    """Test the connection to the desired cluster."""
+def test_connect(isolated_filesystem):
+    """Test the connection to the desired cluster.
+
+    :id: d94f7f5e-9f55-11e9-95da-8c1645a90ee2
+    :description: Test that connection to the yupana application can be made.
+    :expectedresults: At 200 status response should be recieved, indicating a
+        successfull connection.
+    """
     response = requests.get(get_app_url() + "status/")
     assert response.status_code == 200, response.text
 
 
-def test_upload():
-    """Test uploading a file to the service."""
+def test_upload(isolated_filesystem):
+    """Test uploading a file to the service.
+
+    :id: 2bd03a02-9f56-11e9-b9ee-8c1645a90ee2
+    :description: Test that a sample package can be uploaded to the upload
+        service.
+    :expectedresults: At 202 status response should be recieved, indicating a
+        successfull transfer.
+    """
     yupana_config = yupana_configs()
     config = yupana_config["upload-service"]
     response = post_file(
