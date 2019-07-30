c405462d195940f9d2f17173abdd9f70e0721159
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-07-30 09:40:24 -0400

    Switched to using multi search function added to oc.utils (#330)
    * Switched to using multi search function added to oc.utils
    
    * Fixed lint errors. Removed imported packages no longer needed now that functions have been moved to oc.utils

--- camayoc/tests/qpc/yupana/utils.py
+++ camayoc/tests/qpc/yupana/utils.py
@@ -4,14 +4,11 @@
 
 import base64
 import json
-import multiprocessing as mp
 import re
 import requests
 import time
 
 from datetime import datetime
-from functools import partial
-from itertools import chain
 
 from oc import get_pods
 
@@ -109,20 +106,6 @@ def get_timestamp(string, regex=PATTERN_DATE_TIME):
         return datetime(year, month, day, hour=hour, minute=min, second=sec)
     else:
         return None
-
-
-def search_log(log_list, search_string):
-    """Search through a log list for lines containing the `search_string`.
-        Returns a list of the matched lines."""
-    return [log_line for log_line in log_list if search_string in log_line]
-
-
-def search_mult_pod_logs(pods_logs, search_string, pool_size=4):
-    """Search through the logs of each pod, and return the combined list."""
-    search_func = partial(search_log, search_string=search_string)
-    with mp.Pool(pool_size) as p:
-        search_results = p.map(search_func, pods_logs)
-    return list(chain(search_results))
 
 
 def filter_log(
