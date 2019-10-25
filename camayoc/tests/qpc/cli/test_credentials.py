94357354b44031b548e86ba89c16091c68cf5838
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-10-25 13:58:46 -0400

    Removed sshkeyfile resolve commands

--- camayoc/tests/qpc/cli/test_credentials.py
+++ camayoc/tests/qpc/cli/test_credentials.py
@@ -135,17 +135,20 @@ def test_add_with_username_sshkeyfile(isolated_filesystem, qpc_server_config):
     """
     name = utils.uuid4()
     username = utils.uuid4()
-    #sshkeyfile = Path(utils.uuid4())
-    #sshkeyfile.touch()
+    # sshkeyfile = Path(utils.uuid4())
+    # sshkeyfile.touch()
     sshkeyfile = '/sshkeys/id_rsa'
+
     cred_add_and_check(
-        {"name": name, "username": username, "sshkeyfile": str(sshkeyfile.resolve())}
+# {"name": name, "username": username, "sshkeyfile": str(sshkeyfile.resolve())}
+        {"name": name, "username": username, "sshkeyfile": sshkeyfile}
     )
 
     cred_show_and_check(
         {"name": name},
         generate_show_output(
-            {"name": name, "ssh_keyfile": sshkeyfile.resolve(), "username": username}
+# {"name": name, "ssh_keyfile": sshkeyfile.resolve(), "username": username}
+            {"name": name, "ssh_keyfile": sshkeyfile, "username": username}
         ),
     )
 
