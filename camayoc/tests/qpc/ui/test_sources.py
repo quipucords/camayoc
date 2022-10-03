61f971475a737f32a491d3c9fb5ea5a74308656e
Ruda Moura <rmoura@redhat.com>
2022-10-03 10:56:01 -0300

    ui: port old tests to the new UI test framework
    Ported old tests based on Selenium framework to
    the new UI frameworkd, based on Playwright.
    
    * test_login
    * test_credentials
    * test_sources

--- camayoc/tests/qpc/ui/test_sources.py
+++ camayoc/tests/qpc/ui/test_sources.py
@@ -11,25 +11,37 @@
 """
 import pytest
 
-from .utils import create_source
-from .utils import delete_source
-from camayoc import utils
+from camayoc.qpc_models import Credential
+from camayoc.qpc_models import Source
+from camayoc.ui import Client
+from camayoc.ui import data_factories
+from camayoc.ui.data_factories import AddCredentialDTOFactory
+from camayoc.ui.data_factories import AddSourceDTOFactory
+from camayoc.ui.enums import CredentialTypes
+from camayoc.ui.enums import MainMenuPages
+from camayoc.ui.enums import SourceTypes
 
 SOURCE_DATA = {
-    "Network": [
+    SourceTypes.NETWORK_RANGE: [
         "127.0.0.1",
         "127.0.0.1, 127.0.0.2",
         "192.168.1.[1:100]",
         "192.168.0.0/24",
         "example.sonar.com",
     ],
-    "Satellite": ["127.0.0.1", "examplesatellite.sonar.com"],
-    "VCenter": ["127.0.0.1", "examplevcenter.sonar.com"],
+    SourceTypes.SATELLITE: ["127.0.0.1", "examplesatellite.sonar.com"],
+    SourceTypes.VCENTER_SERVER: ["127.0.0.1", "examplevcenter.sonar.com"],
+}
+
+MATCHING_TYPES = {
+    SourceTypes.NETWORK_RANGE: CredentialTypes.NETWORK,
+    SourceTypes.SATELLITE: CredentialTypes.SATELLITE,
+    SourceTypes.VCENTER_SERVER: CredentialTypes.VCENTER,
 }
 
 
 @pytest.mark.parametrize("source_type, ", SOURCE_DATA.keys())
-def test_create_delete_source(selenium_browser, qpc_login, credentials, source_type):
+def test_create_delete_source(ui_client: Client, source_type, cleanup):
     """Create and then delete a source through the UI.
 
     :id: b1f64fd6-0421-4650-aa6d-149cb3099012
@@ -41,15 +53,21 @@ def test_create_delete_source(selenium_browser, qpc_login, credentials, source_t
     :expectedresults: A new source is created with the provided information,
         then it is deleted.
     """
-    #  A dict of source names with the address parameters as keys is created.
-    source_names = {}
-    #  Credentials have to be the same type as the source, so we use pre-made
-    #  credentials that exist for each type.
-    credential_name = credentials[source_type]
-    for addresses in SOURCE_DATA[source_type]:
-        source_name = utils.uuid4()
-        source_names[addresses] = source_name
-        create_source(selenium_browser, credential_name, source_type, source_name, addresses)
-    #  Deletion internally asserts all new sources exist in the UI.
-    for addresses in SOURCE_DATA[source_type]:
-        delete_source(selenium_browser, source_names[addresses])
+
+    credential_type = MATCHING_TYPES[source_type]
+    credential_data = AddCredentialDTOFactory(credential_type=credential_type)
+    source_data = AddSourceDTOFactory(
+        select_source_type__source_type=source_type,
+        source_form__credentials=[credential_data.credential_form_dto.credential_name],
+    )
+    cleanup.append(Credential(name=credential_data.credential_form_dto.credential_name))
+    cleanup.append(Source(name=source_data.source_form.source_name))
+    (
+        ui_client.begin()
+        .login(data_factories.LoginFormDTOFactory())
+        .navigate_to(MainMenuPages.CREDENTIALS)
+        .add_credential(credential_data)
+        .navigate_to(MainMenuPages.SOURCES)
+        .add_source(source_data)
+        .logout()
+    )
