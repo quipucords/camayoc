37a8111f338ac6e356bf918ed275003a33818fc5
Ruda Moura <rmoura@redhat.com>
2022-10-03 10:56:01 -0300

    ui: port old tests to the new UI test framework
    Ported old tests based on Selenium framework to
    the new UI frameworkd, based on Playwright.
    
    * test_login
    * test_credentials
    * test_sources

--- camayoc/tests/qpc/ui/test_credentials.py
+++ camayoc/tests/qpc/ui/test_credentials.py
@@ -9,21 +9,80 @@
 :testtype: functional
 :upstream: yes
 """
-from pathlib import Path
+import random
 
 import pytest
 
-from .utils import check_auth_type
-from .utils import create_credential
-from .utils import delete_credential
-from .utils import edit_credential
-from camayoc import utils
+from camayoc.qpc_models import Credential
+from camayoc.ui import Client
+from camayoc.ui import data_factories
+from camayoc.ui.enums import CredentialTypes
+from camayoc.ui.enums import MainMenuPages
+from camayoc.ui.enums import NetworkCredentialBecomeMethods
 
-SOURCE_TYPES = ["Network", "Satellite", "VCenter"]
+
+def credentials_provider():
+    credential_form = data_factories.SSHNetworkCredentialFormDTOFactory(
+        username="user",
+        ssh_key_file="/sshkeys/id_rsa",
+        passphrase="123456",
+        become_method=NetworkCredentialBecomeMethods.SUDO,
+        become_user="root",
+        become_password="power!",
+    )
+    credential = data_factories.AddCredentialDTOFactory(
+        credential_type=CredentialTypes.NETWORK,
+        credential_form_dto=credential_form,
+    )
+    yield pytest.param(credential, id="network-ssh")
+    credential_form = data_factories.PlainNetworkCredentialFormDTOFactory(
+        username="cloud-user",
+        password="123456",
+        become_method=NetworkCredentialBecomeMethods.SUDO,
+        become_user="root",
+        become_password="power!",
+    )
+    credential = data_factories.AddCredentialDTOFactory(
+        credential_type=CredentialTypes.NETWORK,
+        credential_form_dto=credential_form,
+    )
+    yield pytest.param(credential, id="network-plain")
+    credential = data_factories.AddCredentialDTOFactory(credential_type=CredentialTypes.SATELLITE)
+    yield pytest.param(credential, id="satellite")
+    credential = data_factories.AddCredentialDTOFactory(credential_type=CredentialTypes.VCENTER)
+    yield pytest.param(credential, id="vcenter")
 
 
-@pytest.mark.parametrize("source_type", SOURCE_TYPES)
-def test_create_delete_credential(selenium_browser, qpc_login, source_type):
+def network_credentials_provider():
+    credential_form = data_factories.SSHNetworkCredentialFormDTOFactory(
+        username="cloud-user",
+        ssh_key_file="/sshkeys/id_rsa",
+        passphrase="123456",
+        become_method=random.choice(tuple(NetworkCredentialBecomeMethods)),
+        become_user="superuser",
+        become_password="12345678",
+    )
+    credential = data_factories.AddCredentialDTOFactory(
+        credential_type=CredentialTypes.NETWORK,
+        credential_form_dto=credential_form,
+    )
+    yield pytest.param(credential, id="network-ssh")
+    credential_form = data_factories.PlainNetworkCredentialFormDTOFactory(
+        username="cloud-user",
+        password="123456",
+        become_method=random.choice(tuple(NetworkCredentialBecomeMethods)),
+        become_user="superuser",
+        become_password="12345678",
+    )
+    credential = data_factories.AddCredentialDTOFactory(
+        credential_type=CredentialTypes.NETWORK,
+        credential_form_dto=credential_form,
+    )
+    yield pytest.param(credential, id="network-plain")
+
+
+@pytest.mark.parametrize("credential", credentials_provider())
+def test_create_delete_credential(ui_client: Client, credential, cleanup):
     """Create and then delete a credential in the quipucords UI.
 
     :id: d9fd61f5-1e8e-4091-b8c5-bc787884c6be
@@ -35,22 +94,18 @@ def test_create_delete_credential(selenium_browser, qpc_login, source_type):
         4) Delete the newly created credential.
     :expectedresults: A new credential is created and then deleted.
     """
-    name = utils.uuid4()
-    username = utils.uuid4()
-    password = utils.uuid4()
-    create_credential(
-        selenium_browser,
-        {
-            "name": name,
-            "username": username,
-            "password": password,
-            "source_type": source_type,
-        },
+    cleanup.append(Credential(name=credential.credential_form_dto.credential_name))
+    (
+        ui_client.begin()
+        .login(data_factories.LoginFormDTOFactory())
+        .navigate_to(MainMenuPages.CREDENTIALS)
+        .add_credential(credential)
+        .logout()
     )
-    delete_credential(selenium_browser, {name})
 
 
-def test_create_delete_credential_optional(selenium_browser, qpc_login):
+@pytest.mark.parametrize("credential", network_credentials_provider())
+def test_create_delete_credential_optional(ui_client: Client, credential, cleanup):
     """Create and then delete a credential with optional parameters.
 
     :id: 37632616-86e9-47d1-b1f6-78dd5dde0774
@@ -64,155 +119,47 @@ def test_create_delete_credential_optional(selenium_browser, qpc_login):
         4) Delete the newly created credential.
     :expectedresults: A new credential is created and then deleted.
     """
-    name = utils.uuid4()
-    username = utils.uuid4()
-    password = utils.uuid4()
-    become_user = utils.uuid4()
-    become_pass = utils.uuid4()
-    create_credential(
-        selenium_browser,
-        {
-            "name": name,
-            "username": username,
-            "password": password,
-            "source_type": "Network",
-            "become_user": become_user,
-            "become_pass": become_pass,
-        },
+    cleanup.append(Credential(name=credential.credential_form_dto.credential_name))
+    (
+        ui_client.begin()
+        .login(data_factories.LoginFormDTOFactory())
+        .navigate_to(MainMenuPages.CREDENTIALS)
+        .add_credential(credential)
+        .logout()
     )
-    delete_credential(selenium_browser, {name})
 
 
-def test_create_delete_credential_sshkey(isolated_filesystem, selenium_browser, qpc_login):
-    """Create and then delete a credential using an sshkey file.
+# @pytest.mark.parametrize("source_type", SOURCE_TYPES)
+# def test_edit_credential(ui_client: Client, source_type):
+#     """Edit a credential's parameters.
 
-    :id: 5ec5847c-6d41-4e4a-9f22-cc433eb11078
-    :description: An SSH keyfile is created and used in this test
-        to create a credential, which is deleted afterwards.
-    :steps:
-        1) Log into the UI.
-        2) Go to the credentials page and open the Add Credential modal.
-        3) Fill in required fields, using the SSH key option and save.
-        4) Delete the newly created credential.
-    :expectedresults: A new credential is created and then deleted.
-    """
-    name = utils.uuid4()
-    username = utils.uuid4()
-    sshkeyfile = Path(utils.uuid4())
-    sshkeyfile.touch()
-    passphrase = utils.uuid4()
-    create_credential(
-        selenium_browser,
-        {
-            "name": name,
-            "username": username,
-            "sshkeyfile": str(sshkeyfile.resolve()),
-            "passphrase": passphrase,
-            "source_type": "Network",
-        },
-    )
-    check_auth_type(name, "SSH Key")
-    delete_credential(selenium_browser, {name})
-
-
-def test_credential_sshkey_optional(isolated_filesystem, selenium_browser, qpc_login):
-    """Create/delete a credential that uses an sshkey file and optional parameters.
-
-    :id: a602ab9b-ee76-45fd-bbb2-f6f074c66819
-    :description: All optional parameters and an SSH key are used to create a
-        credential. Afterwards, the credential is deleted.
-    :steps:
-        1) Log into the UI.
-        2) Go to the credentials page and open the Add Credential modal.
-        3) Fill required + optional fields, using the SSH key option and save.
-        4) Delete the newly created credential.
-    :expectedresults: A new credential is created and then deleted.
-    """
-    name = utils.uuid4()
-    username = utils.uuid4()
-    sshkeyfile = Path(utils.uuid4())
-    sshkeyfile.touch()
-    passphrase = utils.uuid4()
-    become_user = utils.uuid4()
-    become_pass = utils.uuid4()
-    create_credential(
-        selenium_browser,
-        {
-            "name": name,
-            "username": username,
-            "sshkeyfile": str(sshkeyfile.resolve()),
-            "passphrase": passphrase,
-            "source_type": "Network",
-            "become_user": become_user,
-            "become_pass": become_pass,
-        },
-    )
-    check_auth_type(name, "SSH Key")
-    delete_credential(selenium_browser, {name})
-
-
-@pytest.mark.parametrize("source_type", SOURCE_TYPES)
-def test_edit_credential(selenium_browser, qpc_login, source_type):
-    """Edit a credential's parameters.
-
-    :id: 3a4626ab-c667-41dc-944e-62ac05d51bbe
-    :description: Creates a credential, then attempts to edit all of the
-        parameters that were set, then verifies that they were changed.
-    :steps:
-        1) Log into the UI.
-        2) Go to the credentials page and open the Add Credential modal.
-        3) Fill required + optional fields, using the password option and save.
-        4) Edit the credential parameters and save, make sure it changed.
-    :expectedresults: A new credential is created and then edited.
-    """
-    options = {
-        "name": utils.uuid4(),
-        "username": utils.uuid4(),
-        "password": utils.uuid4(),
-        "source_type": source_type,
-    }
-    if source_type == "Network":
-        options["become_user"] = utils.uuid4()
-    create_credential(selenium_browser, options)
-    new_options = {
-        "name": utils.uuid4(),
-        "username": utils.uuid4(),
-        "password": utils.uuid4(),
-        "source_type": source_type,
-    }
-    if source_type == "Network":
-        new_options["become_user"] = utils.uuid4()
-    edit_credential(selenium_browser, options["name"], new_options)
-    delete_credential(selenium_browser, {new_options["name"]})
-
-
-def test_edit_credential_sshkey(selenium_browser, qpc_login, isolated_filesystem):
-    """Edit the sshkeyfile of a credential.
-
-    :id: 77281d4c-de9f-4db9-9a9f-0c78b0d3811e
-    :description: Creates a credential, then attempts to edit the sshkeyfile
-        to use a new one. The edit is then verified.
-    :steps:
-        1) Log into the UI.
-        2) Go to the credentials page and open the Add Credential modal.
-        3) Fill required fields, using the SSH key option and save.
-        4) Edit the credential parameters and save, make sure it changed.
-    :expectedresults: A new credential is created and then edited.
-    """
-    sshkeyfile = Path(utils.uuid4())
-    sshkeyfile.touch()
-    options = {
-        "name": utils.uuid4(),
-        "username": utils.uuid4(),
-        "sshkeyfile": str(sshkeyfile.resolve()),
-        "source_type": "Network",
-    }
-    create_credential(selenium_browser, options)
-    new_sshkeyfile = Path(utils.uuid4())
-    new_sshkeyfile.touch()
-    new_options = {
-        "sshkeyfile": str(new_sshkeyfile.resolve()),
-        "source_type": "Network",
-    }
-    edit_credential(selenium_browser, options["name"], new_options)
-    delete_credential(selenium_browser, {options["name"]})
+#     :id: 3a4626ab-c667-41dc-944e-62ac05d51bbe
+#     :description: Creates a credential, then attempts to edit all of the
+#         parameters that were set, then verifies that they were changed.
+#     :steps:
+#         1) Log into the UI.
+#         2) Go to the credentials page and open the Add Credential modal.
+#         3) Fill required + optional fields, using the password option and save.
+#         4) Edit the credential parameters and save, make sure it changed.
+#     :expectedresults: A new credential is created and then edited.
+#     """
+#     # options = {
+#     #     "name": utils.uuid4(),
+#     #     "username": utils.uuid4(),
+#     #     "password": utils.uuid4(),
+#     #     "source_type": source_type,
+#     # }
+#     # if source_type == "Network":
+#     #     options["become_user"] = utils.uuid4()
+#     # create_credential(selenium_browser, options)
+#     # new_options = {
+#     #     "name": utils.uuid4(),
+#     #     "username": utils.uuid4(),
+#     #     "password": utils.uuid4(),
+#     #     "source_type": source_type,
+#     # }
+#     # if source_type == "Network":
+#     #     new_options["become_user"] = utils.uuid4()
+#     # edit_credential(selenium_browser, options["name"], new_options)
+#     # delete_credential(selenium_browser, {new_options["name"]})
+#     pass
--- camayoc/tests/qpc/ui/test_login.py
+++ camayoc/tests/qpc/ui/test_login.py
@@ -9,12 +9,11 @@
 :testtype: functional
 :upstream: yes
 """
-from .utils import wait_for_animation
-from .views import DashboardView
-from .views import LoginView
+from camayoc.ui import Client
+from camayoc.ui import data_factories
 
 
-def test_login_logout(selenium_browser):
+def test_login_logout(ui_client: Client):
     """Login and logout using the default user.
 
     :id: 88bbf267-d32e-44b1-934f-e69c84e5c99d
@@ -26,15 +25,6 @@ def test_login_logout(selenium_browser):
         3) Logout and assert that the login page is shown.
     :expectedresults: Both login and logout must work.
     """
-    login = LoginView(selenium_browser)
-    login.username.fill("admin")
-    login.password.fill("pass")
-    login.login.click()
-
-    assert selenium_browser.selenium.title == "Entitlements Reporting"
-
-    dashboard = DashboardView(selenium_browser)
-    wait_for_animation()
-    dashboard.user_dropdown.select_item("Log out")
-
-    login.login.wait_displayed()
+    page = ui_client.begin().login(data_factories.LoginFormDTOFactory())
+    assert page._driver.title() in ("Quipucords", "product discovery")
+    page.logout()
