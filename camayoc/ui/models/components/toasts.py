6b57abb5d44e2e8a23f3fdb209734e47a0b01804
Mirek Długosz <mzalewsk@redhat.com>
2023-08-16 14:51:05 +0200

    Fix element selectors for login, toasts and scan download

--- camayoc/ui/models/components/toasts.py
+++ camayoc/ui/models/components/toasts.py
@@ -9,12 +9,14 @@ class ToastNotifications(UIPage):
     def _dismiss_notifications(self, ensure_notifications_appeared=False):
         default_timeout = 100  # milliseconds!
         current_timeout = 5000 if ensure_notifications_appeared else default_timeout
-        notifications_selector = ".toast-notifications-list-pf .toast-pf"
+        notifications_selector = ".pf-m-toast > li > .pf-c-alert"
 
         notifications = self._driver.locator(notifications_selector)
         while True:
             try:
-                notifications.nth(0).locator("button.close").click(timeout=current_timeout)
+                notifications.nth(0).locator("button[aria-label^=Close]").click(
+                    timeout=current_timeout
+                )
                 current_timeout = default_timeout
             except TimeoutError:
                 break
