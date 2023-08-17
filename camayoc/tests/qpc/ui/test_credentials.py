38d5298a987b449553a5089947ca87723274b3c2
Mirek Długosz <mzalewsk@redhat.com>
2023-08-17 16:54:14 +0200

    Port UI credential tests to data provider
    This gives us an opportunity to remove created data automatically before
    running next set of tests.

--- camayoc/tests/qpc/ui/test_credentials.py
+++ camayoc/tests/qpc/ui/test_credentials.py
@@ -8,8 +8,10 @@
 :testtype: functional
 """
 import random
+from typing import get_args
 
 import pytest
+from littletable import Table
 
 from camayoc.qpc_models import Credential
 from camayoc.ui import Client
@@ -17,70 +19,60 @@ from camayoc.ui import data_factories
 from camayoc.ui.enums import CredentialTypes
 from camayoc.ui.enums import MainMenuPages
 from camayoc.ui.enums import NetworkCredentialBecomeMethods
+from camayoc.ui.types import CredentialFormDTO
+from camayoc.ui.types import NetworkCredentialFormDTO
+from camayoc.ui.types import PlainNetworkCredentialFormDTO
+from camayoc.ui.types import SatelliteCredentialFormDTO
+from camayoc.ui.types import SSHNetworkCredentialFormDTO
+from camayoc.ui.types import VCenterCredentialFormDTO
+
+CREDENTIAL_TYPE_MAP = {
+    SatelliteCredentialFormDTO: CredentialTypes.SATELLITE,
+    VCenterCredentialFormDTO: CredentialTypes.VCENTER,
+}
 
 
-def credentials_provider():
-    credential_form = data_factories.SSHNetworkCredentialFormDTOFactory(
-        username="user",
-        ssh_key_file="/sshkeys/id_rsa",
-        passphrase="123456",
-        become_method=NetworkCredentialBecomeMethods.SUDO,
-        become_user="root",
-        become_password="power!",
-    )
+def create_credential_dto(credential_type, data_provider):
+    if issubclass(credential_type, get_args(NetworkCredentialFormDTO)):
+        factory_kwargs = {
+            "username": "user",
+            "become_method": random.choice(tuple(NetworkCredentialBecomeMethods)),
+            "become_user": "root",
+            "become_password": "power!",
+        }
+        form_factory_cls = getattr(data_factories, f"{credential_type.__name__}Factory")
+        if issubclass(credential_type, PlainNetworkCredentialFormDTO):
+            form_factory_cls = data_factories.PlainNetworkCredentialFormDTOFactory
+            factory_kwargs["password"] = "123456"
+        if issubclass(credential_type, SSHNetworkCredentialFormDTO):
+            form_factory_cls = data_factories.SSHNetworkCredentialFormDTOFactory
+            ssh_network_credential = data_provider.credentials.new_one(
+                {"type": "network", "sshkeyfile": Table.is_not_null()},
+                data_only=True,
+            )
+            ssh_factory_kwargs = {
+                "ssh_key_file": ssh_network_credential.ssh_keyfile,
+                "passphrase": "123456",
+            }
+            factory_kwargs.update(ssh_factory_kwargs)
+        credential_form = form_factory_cls(**factory_kwargs)
+        credential = data_factories.AddCredentialDTOFactory(
+            credential_type=CredentialTypes.NETWORK,
+            credential_form_dto=credential_form,
+        )
+        data_provider.mark_for_cleanup(Credential(name=credential_form.credential_name))
+        return credential
+
     credential = data_factories.AddCredentialDTOFactory(
-        credential_type=CredentialTypes.NETWORK,
-        credential_form_dto=credential_form,
+        credential_type=CREDENTIAL_TYPE_MAP.get(credential_type)
     )
-    yield pytest.param(credential, id="network-ssh")
-    credential_form = data_factories.PlainNetworkCredentialFormDTOFactory(
-        username="cloud-user",
-        password="123456",
-        become_method=NetworkCredentialBecomeMethods.SUDO,
-        become_user="root",
-        become_password="power!",
-    )
-    credential = data_factories.AddCredentialDTOFactory(
-        credential_type=CredentialTypes.NETWORK,
-        credential_form_dto=credential_form,
-    )
-    yield pytest.param(credential, id="network-plain")
-    credential = data_factories.AddCredentialDTOFactory(credential_type=CredentialTypes.SATELLITE)
-    yield pytest.param(credential, id="satellite")
-    credential = data_factories.AddCredentialDTOFactory(credential_type=CredentialTypes.VCENTER)
-    yield pytest.param(credential, id="vcenter")
+    data_provider.mark_for_cleanup(Credential(name=credential.credential_form_dto.credential_name))
+    return credential
 
 
-def network_credentials_provider():
-    credential_form = data_factories.SSHNetworkCredentialFormDTOFactory(
-        username="cloud-user",
-        ssh_key_file="/sshkeys/id_rsa",
-        passphrase="123456",
-        become_method=random.choice(tuple(NetworkCredentialBecomeMethods)),
-        become_user="superuser",
-        become_password="12345678",
-    )
-    credential = data_factories.AddCredentialDTOFactory(
-        credential_type=CredentialTypes.NETWORK,
-        credential_form_dto=credential_form,
-    )
-    yield pytest.param(credential, id="network-ssh")
-    credential_form = data_factories.PlainNetworkCredentialFormDTOFactory(
-        username="cloud-user",
-        password="123456",
-        become_method=random.choice(tuple(NetworkCredentialBecomeMethods)),
-        become_user="superuser",
-        become_password="12345678",
-    )
-    credential = data_factories.AddCredentialDTOFactory(
-        credential_type=CredentialTypes.NETWORK,
-        credential_form_dto=credential_form,
-    )
-    yield pytest.param(credential, id="network-plain")
-
-
-@pytest.mark.parametrize("credential", credentials_provider())
-def test_create_delete_credential(ui_client: Client, credential, cleanup):
+# FIXME: this never actually deletes in UI
+@pytest.mark.parametrize("credential_type", get_args(CredentialFormDTO))
+def test_create_delete_credential(data_provider, ui_client: Client, credential_type):
     """Create and then delete a credential in the quipucords UI.
 
     :id: d9fd61f5-1e8e-4091-b8c5-bc787884c6be
@@ -92,37 +84,12 @@ def test_create_delete_credential(ui_client: Client, credential, cleanup):
         4) Delete the newly created credential.
     :expectedresults: A new credential is created and then deleted.
     """
-    cleanup.append(Credential(name=credential.credential_form_dto.credential_name))
+    credential_dto = create_credential_dto(credential_type, data_provider)
     (
         ui_client.begin()
         .login(data_factories.LoginFormDTOFactory())
         .navigate_to(MainMenuPages.CREDENTIALS)
-        .add_credential(credential)
-        .logout()
-    )
-
-
-@pytest.mark.parametrize("credential", network_credentials_provider())
-def test_create_delete_credential_optional(ui_client: Client, credential, cleanup):
-    """Create and then delete a credential with optional parameters.
-
-    :id: 37632616-86e9-47d1-b1f6-78dd5dde0774
-    :description: Optional parameters are included in this test,
-        like Become User and Become Password. Afterwards, the new
-        credential is deleted.
-    :steps:
-        1) Log into the UI.
-        2) Go to the credentials page and open the Add Credential modal.
-        3) Fill in required and optional fields and save.
-        4) Delete the newly created credential.
-    :expectedresults: A new credential is created and then deleted.
-    """
-    cleanup.append(Credential(name=credential.credential_form_dto.credential_name))
-    (
-        ui_client.begin()
-        .login(data_factories.LoginFormDTOFactory())
-        .navigate_to(MainMenuPages.CREDENTIALS)
-        .add_credential(credential)
+        .add_credential(credential_dto)
         .logout()
     )
 
