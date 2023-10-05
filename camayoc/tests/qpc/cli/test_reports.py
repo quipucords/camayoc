4b4b2a1dffc272f487f760aa24c33570b8dd0eec
Nicole Aragao <nicolearagao@gmail.com>
2023-10-05 12:56:55 -0300

    test(acs): add acs-detection to report tests

--- camayoc/tests/qpc/cli/test_reports.py
+++ camayoc/tests/qpc/cli/test_reports.py
@@ -87,6 +87,7 @@ DEPLOYMENTS_REPORT_FIELDS = (
 """Common deployments report expected fields."""
 
 CSV_DEPLOYMENTS_REPORT_FIELDS = DEPLOYMENTS_REPORT_FIELDS + (
+    "detection-acs",
     "detection-ansible",
     "detection-network",
     "detection-openshift",
