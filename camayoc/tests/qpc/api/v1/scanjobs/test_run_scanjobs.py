bd79fb5e0f9299d72426cddfa050a2676d336315
Mirek Długosz <mzalewsk@redhat.com>
2024-04-05 15:05:47 +0200

    Unskip test_scan_complete

--- camayoc/tests/qpc/api/v1/scanjobs/test_run_scanjobs.py
+++ camayoc/tests/qpc/api/v1/scanjobs/test_run_scanjobs.py
@@ -9,19 +9,15 @@ These tests are parametrized on the inventory listed in the config file.
 :caselevel: integration
 :testtype: functional
 """
-from pprint import pformat
-
 import pytest
 
-from camayoc import utils
-from camayoc.tests.qpc.api.v1.conftest import scan_list
-from camayoc.types.settings import ScanOptions
+from camayoc.tests.qpc.utils import all_scan_names
+from camayoc.types.scans import ScanSimplifiedStatusEnum
 
 
-@pytest.mark.skip(reason="Test is flaky. Skipping until Quipucords Issue #2040 resoloved")
 @pytest.mark.runs_scan
-@pytest.mark.parametrize("scan_info", scan_list(), ids=utils.name_getter)
-def test_scan_complete(scan_info: ScanOptions):
+@pytest.mark.parametrize("scan_name", all_scan_names())
+def test_scan_complete(scans, scan_name):
     """Test that each scan completed without failures.
 
     :id: 4e7a608e-a18d-48db-b263-a0d5474385dd
@@ -29,26 +25,21 @@ def test_scan_complete(scan_info: ScanOptions):
     :steps: Check the final status of the scan.
     :expectedresults: Scans should complete and report their finished status.
     """
-
-    def get_scan_result(scan_name):
-        return {}
-
-    result = get_scan_result(scan_info.name)
-    if result["final_status"] != "completed":
-        raise AssertionError(
-            "Scan did not complete. Its final status was {status}.\n"
-            " NOTE: A scan will be reported as failed if there were\n"
-            " unreachable hosts.\n"
-            " ============================================================\n"
-            " Any additional HTTP errors encountered while attempting\n"
-            " to run the scan are listed here: \n"
-            "{errors}\n"
-            " ============================================================\n"
-            " Details from the scan including scan_id, scan_job_id, and the\n"
-            "inspeciton results:\n"
-            "{results}\n".format(
-                status=result["final_status"],
-                errors="\n".join(result["errors"]),
-                results=pformat(result),
-            )
+    all_scans = scans.all()
+    finished_scan = all_scans.get(scan_name)
+    assert finished_scan.status == ScanSimplifiedStatusEnum.COMPLETED, (
+        "Scan did not complete. Its final status was {status}.\n"
+        " NOTE: A scan will be reported as failed if there were\n"
+        " unreachable hosts.\n"
+        " ============================================================\n"
+        " Scan name: {scan_name}\n"
+        " Scan id: {scan_id}\n"
+        " Scan job id: {scanjob_id}\n"
+        " Encountered exception: {error}\n".format(
+            status=finished_scan.status,
+            scan_name=finished_scan.definition.name,
+            scan_id=finished_scan.scan_id,
+            scanjob_id=finished_scan.scan_job_id,
+            error=finished_scan.error,
         )
+    )
