edd276add948d3d75948cafcf6872bc79c1b2393
Mirek Długosz <mzalewsk@redhat.com>
2024-09-25 14:12:26 +0200

    Port login and logout to new UI

--- camayoc/ui/models/pages/login.py
+++ camayoc/ui/models/pages/login.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 from camayoc.types.ui import LoginFormDTO
 from camayoc.ui.decorators import record_action
+from camayoc.ui.enums import MainMenuPages
 from camayoc.ui.enums import Pages
 
 from .abstract_page import AbstractPage
@@ -15,6 +16,9 @@ if TYPE_CHECKING:
 class Login(AbstractPage):
     @record_action
     def login(self, data: LoginFormDTO) -> SourcesMainPage:
+        if self._use_uiv2:
+            return self._login_uiv2(data)
+
         login_page_indicator = "html.login-pf"
         username_input = "input[name=username]"
         password_input = "input[name=password]"
@@ -26,3 +30,22 @@ class Login(AbstractPage):
             self._driver.click(submit_button)
 
         return self._new_page(Pages.SOURCES)
+
+    def _login_uiv2(self, data: LoginFormDTO) -> SourcesMainPage:
+        login_page_indicator = "main[class$=login__main]"
+        username_input = "input[name=pf-login-username-id]"
+        password_input = "input[name=pf-login-password-id]"
+        submit_button = "button[type=submit]"
+
+        if self._driver.locator(login_page_indicator).is_visible():
+            self._driver.fill(username_input, data.username)
+            self._driver.fill(password_input, data.password)
+            self._driver.click(submit_button)
+
+        # The new UI opens Credentials page right after login,
+        # but we need to navigate to Sources to keep interface
+        # compatibility. In most cases it means that we log in,
+        # click Sources in menu and immediately go back to Credentials.
+        # Oh well...
+        credentials_page = self._new_page(Pages.CREDENTIALS)
+        return credentials_page.navigate_to(MainMenuPages.SOURCES)
