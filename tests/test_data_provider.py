c0175b195394afe37aaf6f3964257c3265cc3037
Mirek Długosz <mzalewsk@redhat.com>
2024-12-05 16:57:12 +0100

    Move model from ssh_keyfile to ssh_key
    In qpc_model representing credential, move from ssh_keyfile to ssh_key.
    This also changes the meaning of the field - it used to represent path,
    now it represents the content of the file.
    
    However, when creating model from config definition, it still takes the
    path and reads the content automatically. This way we don't have to
    modify existing configuration files. And storing entire keys in
    configuration files is going to be cumbersome.
    
    However, at some point in the future we should probably streamline all
    that and remove the cruft built around backwards compatibility and past
    solutions.

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -9,13 +9,15 @@ from camayoc.types.settings import SourceOptions
 from camayoc.types.settings import SSHNetworkCredentialOptions
 from camayoc.types.settings import VCenterCredentialOptions
 
+MOCK_SSH_KEY_CONTENT = "--BEGIN OPENSSH PRIVATE KEY--"
+
 CREDENTIALS = [
     SSHNetworkCredentialOptions(
         **{
             "name": "network",
             "type": "network",
             "username": "root",
-            "sshkeyfile": "~/.ssh/id_rsa",
+            "sshkeyfile": "/sshkeys/id_rsa",
         }
     ),
     VCenterCredentialOptions(
@@ -64,63 +66,79 @@ SCANS = [
 
 def test_defined_one():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "create") as mock_create:
-            cred = dp.credentials.defined_one({"type": "network"})
-            mock_create.assert_called_once()
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "create") as mock_create,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        cred = dp.credentials.defined_one({"type": "network"})
+        mock_create.assert_called_once()
     assert cred.name == "network"
     assert cred.username == "root"
-    assert cred.ssh_keyfile == "~/.ssh/id_rsa"
+    assert cred.ssh_key == MOCK_SSH_KEY_CONTENT
 
 
 def test_defined_reuse():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "create") as mock_create:
-            cred1 = dp.credentials.defined_one({"type": "network"})
-            cred2 = dp.credentials.defined_one({"type": "network"})
-            mock_create.assert_called_once()
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "create") as mock_create,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        cred1 = dp.credentials.defined_one({"type": "network"})
+        cred2 = dp.credentials.defined_one({"type": "network"})
+        mock_create.assert_called_once()
     assert cred1.name == "network"
     assert cred1.username == "root"
-    assert cred1.ssh_keyfile == "~/.ssh/id_rsa"
+    assert cred1.ssh_key == MOCK_SSH_KEY_CONTENT
     assert cred1.equivalent(cred2)
 
 
 def test_new_one():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "create") as mock_create:
-            cred = dp.credentials.new_one({"type": "network"}, data_only=False)
-            mock_create.assert_called_once()
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "create") as mock_create,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        cred = dp.credentials.new_one({"type": "network"}, data_only=False)
+        mock_create.assert_called_once()
     assert cred.name != "network"
     assert cred.username == "root"
-    assert cred.ssh_keyfile == "~/.ssh/id_rsa"
+    assert cred.ssh_key == MOCK_SSH_KEY_CONTENT
 
 
 def test_new_one_data_only():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "create") as mock_create:
-            cred = dp.credentials.new_one({"type": "network"}, data_only=True)
-            mock_create.assert_not_called()
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "create") as mock_create,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        cred = dp.credentials.new_one({"type": "network"}, data_only=True)
+        mock_create.assert_not_called()
     assert cred.name != "network"
     assert cred.username == "root"
-    assert cred.ssh_keyfile == "~/.ssh/id_rsa"
+    assert cred.ssh_key == MOCK_SSH_KEY_CONTENT
 
 
 def test_new_with_dependencies():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "create") as mock_cred_create:
-            with mock.patch.object(dp.sources._model_class, "create") as mock_source_create:
-                source1 = dp.sources.new_one(
-                    {"type": "network"}, new_dependencies=True, data_only=False
-                )
-                source2 = dp.sources.new_one(
-                    {"type": "network"}, new_dependencies=True, data_only=False
-                )
-                assert mock_cred_create.call_count == 2
-                assert mock_source_create.call_count == 2
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "create") as mock_cred_create,
+        mock.patch.object(dp.sources._model_class, "create") as mock_source_create,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        source1 = dp.sources.new_one({"type": "network"}, new_dependencies=True, data_only=False)
+        source2 = dp.sources.new_one({"type": "network"}, new_dependencies=True, data_only=False)
+        assert mock_cred_create.call_count == 2
+        assert mock_source_create.call_count == 2
     assert len(dp.credentials._created_models) == 2
     assert source1.name != "mynetwork"
     assert source1.name.startswith("mynetwork")
@@ -129,17 +147,17 @@ def test_new_with_dependencies():
 
 def test_new_without_dependencies():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "create") as mock_cred_create:
-            with mock.patch.object(dp.sources._model_class, "create") as mock_source_create:
-                source1 = dp.sources.new_one(
-                    {"type": "network"}, new_dependencies=False, data_only=False
-                )
-                source2 = dp.sources.new_one(
-                    {"type": "network"}, new_dependencies=False, data_only=False
-                )
-                mock_cred_create.assert_called_once()
-                assert mock_source_create.call_count == 2
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "create") as mock_cred_create,
+        mock.patch.object(dp.sources._model_class, "create") as mock_source_create,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        source1 = dp.sources.new_one({"type": "network"}, new_dependencies=False, data_only=False)
+        source2 = dp.sources.new_one({"type": "network"}, new_dependencies=False, data_only=False)
+        mock_cred_create.assert_called_once()
+        assert mock_source_create.call_count == 2
     assert len(dp.credentials._created_models) == 1
     assert source1.name != "mynetwork"
     assert source1.name.startswith("mynetwork")
@@ -154,26 +172,34 @@ def test_new_no_match():
 
 def test_automatic_cleanup():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "bulk_delete") as mock_delete:
-            cred = dp.credentials.new_one({"type": "network"}, data_only=False)
-            cred._id = 123
-            mock_delete.return_value = mock.Mock
-            mock_delete.return_value.status_code = mock.PropertyMock(return_value=200)
-            dp.cleanup()
-            mock_delete.assert_called()
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "bulk_delete") as mock_delete,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        cred = dp.credentials.new_one({"type": "network"}, data_only=False)
+        cred._id = 123
+        mock_delete.return_value = mock.Mock
+        mock_delete.return_value.status_code = mock.PropertyMock(return_value=200)
+        dp.cleanup()
+        mock_delete.assert_called()
 
 
 def test_mark_for_cleanup():
     dp = DataProvider(credentials=CREDENTIALS, sources=SOURCES, scans=SCANS)
-    with mock.patch("camayoc.api.Client"):
-        with mock.patch.object(dp.credentials._model_class, "bulk_delete") as mock_delete:
-            cred = dp.credentials.new_one({"type": "network"}, data_only=True)
-            cred._id = 123
-            mock_delete.return_value = mock.Mock
-            mock_delete.return_value.status_code = mock.PropertyMock(return_value=200)
-            dp.cleanup()
-            mock_delete.assert_not_called()
-            dp.mark_for_cleanup(cred)
-            dp.cleanup()
-            mock_delete.assert_called()
+    with (
+        mock.patch("camayoc.api.Client"),
+        mock.patch.object(dp.credentials._model_class, "bulk_delete") as mock_delete,
+        mock.patch("camayoc.qpc_models.server_container_ssh_key_content") as mock_ssh_key_content,
+    ):
+        mock_ssh_key_content.return_value = MOCK_SSH_KEY_CONTENT
+        cred = dp.credentials.new_one({"type": "network"}, data_only=True)
+        cred._id = 123
+        mock_delete.return_value = mock.Mock
+        mock_delete.return_value.status_code = mock.PropertyMock(return_value=200)
+        dp.cleanup()
+        mock_delete.assert_not_called()
+        dp.mark_for_cleanup(cred)
+        dp.cleanup()
+        mock_delete.assert_called()
