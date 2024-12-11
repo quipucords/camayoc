cd2f0ffbacad1f3833d4da8212369be34b9f84a7
Mirek Długosz <mzalewsk@redhat.com>
2024-12-11 12:40:59 +0100

    Remove use_uiv2 setting

--- camayoc/config.py
+++ camayoc/config.py
@@ -19,7 +19,6 @@ from camayoc.types.settings import Configuration
 default_dynaconf_validators = [
     Validator("camayoc.run_scans", default=False),
     Validator("camayoc.scan_timeout", default=600),
-    Validator("camayoc.use_uiv2", default=True),
     Validator("camayoc.db_cleanup", default=True),
     Validator("quipucords_server.hostname", default=""),
     Validator("quipucords_server.https", default=False),
--- camayoc/types/settings.py
+++ camayoc/types/settings.py
@@ -12,7 +12,6 @@ from typing_extensions import Annotated
 class CamayocOptions(BaseModel):
     run_scans: Optional[bool] = False
     scan_timeout: Optional[int] = 600
-    use_uiv2: Optional[bool] = True
     db_cleanup: Optional[bool] = True
 
 
