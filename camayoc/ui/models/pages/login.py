dbfeacd4d65bfacd390c0dc1f44609fa2de29447
Mirek Długosz <mzalewsk@redhat.com>
2024-12-11 12:56:59 +0100

    login: stop navigating to Sources to keep interface compatibility

--- camayoc/ui/models/pages/login.py
+++ camayoc/ui/models/pages/login.py
@@ -4,18 +4,17 @@ from typing import TYPE_CHECKING
 
 from camayoc.types.ui import LoginFormDTO
 from camayoc.ui.decorators import record_action
-from camayoc.ui.enums import MainMenuPages
 from camayoc.ui.enums import Pages
 
 from .abstract_page import AbstractPage
 
 if TYPE_CHECKING:
-    from .sources import SourcesMainPage
+    from .credentials import CredentialsMainPage
 
 
 class Login(AbstractPage):
     @record_action
-    def login(self, data: LoginFormDTO) -> SourcesMainPage:
+    def login(self, data: LoginFormDTO) -> CredentialsMainPage:
         login_page_indicator = "main[class$=login__main]"
         username_input = "input[name=pf-login-username-id]"
         password_input = "input[name=pf-login-password-id]"
@@ -26,10 +25,4 @@ class Login(AbstractPage):
             self._driver.fill(password_input, data.password)
             self._driver.click(submit_button)
 
-        # The new UI opens Credentials page right after login,
-        # but we need to navigate to Sources to keep interface
-        # compatibility. In most cases it means that we log in,
-        # click Sources in menu and immediately go back to Credentials.
-        # Oh well...
-        credentials_page = self._new_page(Pages.CREDENTIALS)
-        return credentials_page.navigate_to(MainMenuPages.SOURCES)
+        return self._new_page(Pages.CREDENTIALS)
