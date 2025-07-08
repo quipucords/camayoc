2eeed127c9d0f82fbb444bba74c384b04aa767c7
Mirek Długosz <mzalewsk@redhat.com>
2025-07-08 10:40:56 +0200

    Make CLI login fixture session-scoped

--- camayoc/tests/qpc/cli/conftest.py
+++ camayoc/tests/qpc/cli/conftest.py
@@ -7,7 +7,7 @@ from camayoc.constants import QPC_SOURCE_TYPES
 from .utils import setup_qpc
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def qpc_server_config():
     """Configure and login qpc with Camayoc's configuration info."""
     setup_qpc()
