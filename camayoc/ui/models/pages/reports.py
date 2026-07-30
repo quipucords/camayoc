08f9388269f2dad3f45ebdf08b008be9cfc83f62
Alberto Bellotti <abellott@redhat.com>
2026-07-30 12:37:18 -0400

    tests: Add support for the Reports UI view
    - Adding REPORTS to Pages and MainMenupages
    - Adding the Page object for the Reviews view
    - Added the ReportsMainPage to the vertical navigation
    - Added the new test_reports.py:
      - verifying reports from triggered scans are there
      - verifying downloads a reports via the kebab action and validate the tarball
    
    Resolves: DISCOVERY-1341

---
+++ camayoc/ui/models/pages/reports.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from playwright.sync_api import Download
+
+from camayoc.ui.decorators import creates_toast
+from camayoc.ui.decorators import record_action
+from camayoc.ui.decorators import service
+
+from ..components.items_list import AbstractListItem
+from ..mixins import MainPageMixin
+
+
+class ReportListElem(AbstractListItem):
+    def download_report(self) -> Download:
+        with self._client.driver.expect_download() as download_info:
+            self.select_action("download-report", timeout=10_000)
+        download = download_info.value
+        download.path()
+        return download
+
+
+class ReportsMainPage(MainPageMixin):
+    ITEM_CLASS = ReportListElem
+
+    def _get_item_from_current_list(self, name: str):
+        default_timeout = 5000
+        item_label_locator = "td[data-label='Scan name']"
+        item_elem_locator = "xpath=./ancestor::tr[contains(@class, 'table__tr')]"
+
+        item_elem = self._driver.locator(item_label_locator).filter(
+            has=self._driver.get_by_text(name, exact=True)
+        )
+        item_elem = item_elem.locator(item_elem_locator)
+        item_elem.hover(timeout=default_timeout, trial=True)
+        return item_elem
+
+    def _search_for_item_by_name(self, name: str):
+        filter_field_button_locator = (
+            "div[class*=-c-toolbar__item] button[id]:has(span[class*=-c-menu-toggle])"
+        )
+        filter_field_values_locator = "body > div[class*=-c-menu]"
+
+        filter_field_button = self._driver.locator(filter_field_button_locator).locator("nth=0")
+        if filter_field_button.text_content() != "Scan name":
+            filter_field_button.click()
+            values_list = self._driver.locator(filter_field_values_locator)
+            values_list.locator("text='Scan name'").click()
+        self._driver.fill("input[placeholder$='scan name']", name)
+        self._driver.keyboard.press("Enter")
+
+    @creates_toast
+    @service
+    @record_action
+    def download_report(self, scan_name: str) -> ReportsMainPage:
+        report: ReportListElem = self._get_item(scan_name)
+        downloaded_report = report.download_report()
+        self._client.downloaded_files.append(downloaded_report)
+        return self
+
+    @service
+    @record_action
+    def assert_report_present(self, scan_name: str) -> ReportsMainPage:
+        self._get_item(scan_name)
+        return self
